a75071b4c34b380f5b6433858c217ba1c9bed95b
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-05-13 16:44:29 -0300

    feat: configure entities

--- framework/database/db.go
+++ framework/database/db.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/lib/pq"
 	"github.com/lukinhasssss/encoder-de-video/domain"
 )
 
@@ -19,47 +21,48 @@ type Database struct {
 }
 
 func NewDb() *Database {
-  return &Database{}
+	return &Database{}
 }
 
 func NewDbTest() *gorm.DB {
-  dbInstance := NewDb()
+	dbInstance := NewDb()
 
-  dbInstance.Env = "Test"
-  dbInstance.DbTypeTest = "sqlite3"
-  dbInstance.DsnTest = ":memory:"
-  dbInstance.AutoMigrateDb = true
-  dbInstance.Debug = true
+	dbInstance.Env = "Test"
+	dbInstance.DbTypeTest = "sqlite3"
+	dbInstance.DsnTest = ":memory:"
+	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
 
-  connection, err := dbInstance.Connect()
+	connection, err := dbInstance.Connect()
 
-  if err != nil {
-    log.Fatalf("Test db error: %v", err)
-  }
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
 
-  return connection
+	return connection
 }
 
 func (d *Database) Connect() (*gorm.DB, error) {
-  var err error
+	var err error
 
-  if d.Env != "test" {
-    d.Db, err = gorm.Open(d.DbType, d.Dsn)
-  } else {
-    d.Db, err = gorm.Open(d.DbTypeTest, d.DsnTest)
-  }
+	if d.Env != "test" {
+		d.Db, err = gorm.Open(d.DbType, d.Dsn)
+	} else {
+		d.Db, err = gorm.Open(d.DbTypeTest, d.DsnTest)
+	}
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  if d.Debug {
-    d.Db.LogMode(true)
-  }
+	if d.Debug {
+		d.Db.LogMode(true)
+	}
 
-  if d.AutoMigrateDb {
-    d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
-  }
+	if d.AutoMigrateDb {
+		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+		d.Db.Model(domain.Job{}).AddForeignKey("video_id", "videos (id)", "CASCADE", "CASCADE")
+	}
 
-  return d.Db, nil
+	return d.Db, nil
 }
