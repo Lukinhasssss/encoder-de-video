f45ee54bdf3009cb08bdcf67a8d01dae8fa67905
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-05-22 22:07:32 -0300

    feat: add video encode

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -2,10 +2,12 @@ package services
 
 import (
 	"context"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"path"
 
 	"cloud.google.com/go/storage"
 	"github.com/lukinhasssss/encoder-de-video/application/repositories"
@@ -13,84 +15,136 @@ import (
 )
 
 type VideoService struct {
-  Video *domain.Video
-  VideoRepository repositories.VideoRepository
+	Video           *domain.Video
+	VideoRepository repositories.VideoRepository
 }
 
 func NewVideoService() VideoService {
-  return VideoService{}
+	return VideoService{}
 }
 
 func (v *VideoService) Download(bucketName string) error {
-  ctx := context.Background()
+	ctx := context.Background()
 
-  client, err := storage.NewClient(ctx)
+	client, err := storage.NewClient(ctx)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  bkt := client.Bucket(bucketName)
-  obj := bkt.Object(v.Video.FilePath)
+	bkt := client.Bucket(bucketName)
+	obj := bkt.Object(v.Video.FilePath)
 
-  r, err := obj.NewReader(ctx)
+	r, err := obj.NewReader(ctx)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  defer r.Close()
+	defer r.Close()
 
-  body, err := ioutil.ReadAll(r)
+	body, err := io.ReadAll(r)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  f, err := os.Create(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	f, err := os.Create(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  _, err = f.Write(body)
+	_, err = f.Write(body)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  defer f.Close()
+	defer f.Close()
 
-  log.Printf("video %v has been stored", v.Video.ID)
+	log.Printf("video %v has been stored", v.Video.ID)
 
-  return nil
+	return nil
 }
 
 func (v *VideoService) Fragment() error {
-  err := os.Mkdir(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID, os.ModePerm)
+	err := os.Mkdir(os.Getenv("LOCAL_STORAGE_PATH")+"/"+v.Video.ID, os.ModePerm)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  source := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
-  target := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
+	source := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag"
 
-  cmd := exec.Command("mp4fragment", source, target)
+	cmd := exec.Command("mp4fragment", source, target)
 
-  output, err := cmd.CombinedOutput()
+	output, err := cmd.CombinedOutput()
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return fmt.Errorf("erro ao fragmentar o video: %v", err)
+	}
 
-  printOutput(output)
+	printOutput(output)
 
-  return nil
+	return nil
+}
+
+func (v *VideoService) Encode() error {
+	cmdArgs := []string{}
+	cmdArgs = append(cmdArgs, path.Join(os.Getenv("LOCAL_STORAGE_PATH"), v.Video.ID+".frag"))
+	cmdArgs = append(cmdArgs, "--use-segment-timeline")
+	cmdArgs = append(cmdArgs, "-o")
+	cmdArgs = append(cmdArgs, path.Join(os.Getenv("LOCAL_STORAGE_PATH"), v.Video.ID))
+	cmdArgs = append(cmdArgs, "-f")
+	cmdArgs = append(cmdArgs, "--exec-dir")
+	cmdArgs = append(cmdArgs, "/opt/bento4/bin/")
+
+	cmd := exec.Command("mp4dash", cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Start()
+	if err != nil {
+		return fmt.Errorf("erro ao iniciar o comando: %v", err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("erro ao executar o comando: %v", err)
+	}
+
+	return nil
+}
+
+func (v *VideoService) Finish() error {
+	err := os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".mp4")
+	if err != nil {
+		log.Println("error removing mp4 ", v.Video.ID, ".mp4")
+		return err
+	}
+
+	err = os.Remove(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID + ".frag")
+	if err != nil {
+		log.Println("error removing frag ", v.Video.ID, ".frag")
+		return err
+	}
+
+	err = os.RemoveAll(os.Getenv("LOCAL_STORAGE_PATH") + "/" + v.Video.ID)
+	if err != nil {
+		log.Println("error removing mp4 ", v.Video.ID, ".mp4")
+		return err
+	}
+
+	log.Println("files have been removed: ", v.Video.ID)
+	return nil
+
 }
 
 func printOutput(out []byte) {
-  if len(out) > 0 {
-    log.Printf("======> Output: %s\n", string(out))
-  }
+	if len(out) > 0 {
+		log.Printf("======> Output: %s\n", string(out))
+	}
 }
+
